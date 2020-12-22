0b560b2fc5f06fb8c0b1b8badcf11daef962f91c
Alexey Nikitin <alexeynikitin@outlook.com>
2020-12-22 21:39:39 +0100

    Create Get inserted IDs.sql

---
+++ For Beginners/Get inserted IDs.sql
@@ -0,0 +1,32 @@
+/* Instead of inserting rows into a table and then getting IDs
+   of the inserted rows, you can get the IDs in the same step
+   by using the OUTPUT clause. */
+
+CREATE TABLE #RowsInserted 
+(
+	[Id]		INT
+   ,[Name]		NVARCHAR(40)
+   ,[UserId]	INT
+   ,[Date]		DATETIME
+);
+
+INSERT INTO dbo.Badges
+(
+	[Name]
+   ,[UserId]
+   ,[Date]
+)
+	OUTPUT	INSERTED.[Id]
+		   ,INSERTED.[Name]
+		   ,INSERTED.[UserId]
+		   ,INSERTED.[Date]
+	INTO	#RowsInserted
+
+	SELECT	N'Sunny Disposition'
+		   ,[Id]
+		   ,GETDATE()
+	FROM	dbo.Users
+	WHERE	[Location] = N'Iceland';
+
+SELECT	*
+FROM	#RowsInserted;
